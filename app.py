0090e63fa077d818e6d35f3da29ec29568c7fb78
harika2907 <harikamandadi6@gmail.com>
2025-02-10 23:57:20 +0530

    Hello

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route("/",methods=["GET","POST"])
+def home():
+    
+    if request.method=="POST":
+        msg=request.form.get("message")
+        print(msg)
+    else:
+        return  render_template("index.html",methods=["GET","POST"])
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5153)
